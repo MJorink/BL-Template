2df4b90927bd30f4f86bcf4fd6d25b04e3465f11
Jorink <maxjorink@gmail.com>
2026-08-24 13:50:58 +0200

    Update Template

--- src/ModTemplate.cs
+++ src/ModTemplate.cs
@@ -11,7 +11,8 @@ namespace ModTemplate
 {
     public class Core : MelonMod
     {
-        private static MelonPreferences_Category category;
+        private static MelonPreferences_Entry<bool> exampleBoolEntry;
+        private static MelonPreferences_Entry<float> exampleFloatEntry;
 
         public override void OnInitializeMelon()
         {
@@ -19,18 +20,25 @@ namespace ModTemplate
             SetupBoneMenu();
         }
 
+        private void SetupMelonPreferences()
+        {
+        	MelonPreferences_Category category;
+            category = MelonPreferences.CreateCategory("ModTemplate");
+
+            exampleBoolEntry = category.CreateEntry("Example Bool", false);
+            exampleFloatEntry = category.CreateEntry("Example Float", 0.5f);
+            
+            MelonPreferences.Save();
+            category.SaveToFile();
+        }
+
         private void SetupBoneMenu()
         {
             Page defaultPage = Page.Root.CreatePage("Jorink", Color.red).CreatePage("ModTemplate", Color.yellow);
-            
-            defaultPage.CreateFunction("Save Settings", Color.cyan, () => MelonPreferences.Save());
-        }
 
-        private void SetupMelonPreferences()
-        {
-            category = MelonPreferences.CreateCategory("ModTemplate");
-            MelonPreferences.Save();
-            category.SaveToFile();
+            defaultPage.CreateFloat("Example Float", Color.yellow, exampleFloatEntry.Value, 0.1f, 0.1f, 1f, (value) => exampleFloatEntry.Value = value);
+            defaultPage.CreateBool("Example Bool", Color.yellow, exampleBoolEntry.Value, (value) => exampleBoolEntry.Value = value);
+            defaultPage.CreateFunction("Save Settings", Color.cyan, () => MelonPreferences.Save());
         }
     }
 }
