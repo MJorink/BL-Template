d3a0931305c33c93a8a8fd49488d8b93f46568b1
Jorink <maxjorink@gmail.com>
2026-09-01 22:56:21 +0200

    Fix AssemblyInfo colors

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using template; // Lowercase!
 
 [assembly: MelonInfo(typeof(Template), "Template", Template.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonColor(0,255,0,1)]
-[assembly: MelonAuthorColor(255,0,0,1)]
+[assembly: MelonColor(255,255,255,0)] // ARGB, alpha first. Match Main.cs color!
+[assembly: MelonAuthorColor(255,255,0,0)]
